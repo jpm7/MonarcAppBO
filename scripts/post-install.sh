6021dfa9ff414b55aa0540056d5ddf45f2a58d7b
Guillaume Lesniak <guillaume@netlor.fr>
2016-09-12 11:03:18 +0200

    More symlinks fixup

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -4,7 +4,7 @@
 ln -s vendor/monarc/skeleton app
 mkdir -p app/module
 
-#Link vendors into vendor/monarc/skeleton
+# Link vendors into vendor/monarc/skeleton
 cd vendor/monarc/skeleton
 ln -s ../../../vendor
 
@@ -13,13 +13,21 @@ cd ../../../app/module
 ln -s ../vendor/monarc/backoffice MonarcBO
 ln -s ../vendor/monarc/core MonarcCore
 
-cd ..
+cd ../..
 
 # Link the Angular part of the app
-ln -s $(pwd)/../node_modules node_modules
+NODE_MODULES_PATH=$(pwd)/node_modules
+
+cd node_modules
+npm install grunt-shell
+npm install grunt-angular-gettext
+npm install grunt-contrib-concat
+npm install grunt-contrib-uglify
+
+cd ../app
+ln -s $NODE_MODULES_PATH node_modules
 
 # Compile stuff needed for the minified frontend
-cd app
 ./scripts/compile_translations.sh
 ./scripts/link_modules_resources.sh
 
